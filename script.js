ae2cce0b1a2709cd01d2ab1fcdff3baf0a3cd447
YePyieOo <yepyieoo96@gmail.com>
2022-02-26 23:53:18 +0630

    first

---
+++ script.js
@@ -0,0 +1,182 @@
+// Row(1)
+function key_one() {
+   let x = document.getElementById("one").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+function key_two() {
+   let x = document.getElementById("two").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+function key_three() {
+   let x = document.getElementById("three").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_four() {
+   let x = document.getElementById("four").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_five() {
+   let x = document.getElementById("five").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_six() {
+   let x = document.getElementById("six").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_seven() {
+   let x = document.getElementById("seven").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_eight() {
+   let x = document.getElementById("eight").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_nine() {
+   let x = document.getElementById("nine").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_zero() {
+   let x = document.getElementById("zero").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+// Row(2)
+function key_q1() {
+   let x = document.getElementById("q1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_w1() {
+   let x = document.getElementById("w1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_e1() {
+   let x = document.getElementById("e1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_r1() {
+   let x = document.getElementById("r1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_t1() {
+   let x = document.getElementById("t1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_y1() {
+   let x = document.getElementById("y1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_u1() {
+   let x = document.getElementById("u1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_i1() {
+   let x = document.getElementById("i1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_o1() {
+   let x = document.getElementById("o1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_p1() {
+   let x = document.getElementById("p1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+// Row(3)
+function key_a1() {
+   let x = document.getElementById("a1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+function key_s1() {
+   let x = document.getElementById("s1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_d1() {
+   let x = document.getElementById("d1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_f1() {
+   let x = document.getElementById("f1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_g1() {
+   let x = document.getElementById("g1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_h1() {
+   let x = document.getElementById("h1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_j1() {
+   let x = document.getElementById("j1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_k1() {
+   let x = document.getElementById("k1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_l1() {
+   let x = document.getElementById("l1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+// Row(4)
+function key_z1() {
+   let x = document.getElementById("z1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_x1() {
+   let x = document.getElementById("x1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_c1() {
+   let x = document.getElementById("c1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_v1() {
+   let x = document.getElementById("v1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_b1() {
+   let x = document.getElementById("b1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_n1() {
+   let x = document.getElementById("n1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+function key_m1() {
+   let x = document.getElementById("m1").innerHTML;
+   document.getElementById("show-box").innerHTML += x;
+}
+
+// Clear-key
+function clear_key() {
+   document.getElementById("show-box").innerHTML = "";
+}
+// charAt()
+function charAt() {
+   let x = document.getElementById("show-box").innerHTML;
+   let result = document.getElementById("input").values;
+   console.log = x.charAt(result);
+   alert(x.charAt(result));
+}
+
+// function space_key() {
+//    let x = "";
+//    document.getElementById("show-box").innerHTML = x;
+// }
+
+// function charCodeAt() {}
+
+// function toUpperCaes() {}
+
+// function toLowercase() {}
+// function trim() {}
+// function includes() {}
+// function indexOf() {}
+// function lastIndexOf() {}
+// function startsWith() {}
+// function endsWith() {}
